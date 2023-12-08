249ea125e534320df11c2113a6da808afe4cb41f
Moh097 <mohammadmasri097@gmail.com>
2023-12-08 14:37:40 +0300

    init

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="rectangle" />
+        <div className="rectangle-2" />
+        <div className="rectangle-3" />
+        <div className="rectangle-4" />
+        <div className="rectangle-5" />
+        <div className="rectangle-6" />
+        <div className="text-wrapper">Box 6</div>
+        <div className="text-wrapper-2">Box 5</div>
+        <div className="text-wrapper-3">Box 4</div>
+        <div className="text-wrapper-4">Box 3</div>
+        <div className="text-wrapper-5">Box 2</div>
+        <div className="text-wrapper-6">Box 1</div>
+        <img
+          className="whatsapp-image"
+          alt="Whatsapp image"
+          src="https://cdn.animaapp.com/projects/6572e31b163d7a7791ee9861/releases/6572e371cd9388f80f786a26/img/whatsapp-image-2023-12-08-at-11-31-1.png"
+        />
+        <img
+          className="img"
+          alt="Whatsapp image"
+          src="https://cdn.animaapp.com/projects/6572e31b163d7a7791ee9861/releases/6572e371cd9388f80f786a26/img/whatsapp-image-2023-12-08-at-11-31-1.png"
+        />
+        <img
+          className="whatsapp-image-2"
+          alt="Whatsapp image"
+          src="https://cdn.animaapp.com/projects/6572e31b163d7a7791ee9861/releases/6572e371cd9388f80f786a26/img/whatsapp-image-2023-12-08-at-11-31-1.png"
+        />
+        <img
+          className="whatsapp-image-3"
+          alt="Whatsapp image"
+          src="https://cdn.animaapp.com/projects/6572e31b163d7a7791ee9861/releases/6572e371cd9388f80f786a26/img/whatsapp-image-2023-12-08-at-11-31-1.png"
+        />
+        <img
+          className="whatsapp-image-4"
+          alt="Whatsapp image"
+          src="https://cdn.animaapp.com/projects/6572e31b163d7a7791ee9861/releases/6572e371cd9388f80f786a26/img/whatsapp-image-2023-12-08-at-11-31-1.png"
+        />
+        <img
+          className="whatsapp-image-5"
+          alt="Whatsapp image"
+          src="https://cdn.animaapp.com/projects/6572e31b163d7a7791ee9861/releases/6572e371cd9388f80f786a26/img/whatsapp-image-2023-12-08-at-11-31-1.png"
+        />
+        <div className="overlap-group">
+          <div className="overlap">
+            <img
+              className="line"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/6572e31b163d7a7791ee9861/releases/6572e371cd9388f80f786a26/img/line-1.svg"
+            />
+          </div>
+          <img
+            className="download-removebg"
+            alt="Download removebg"
+            src="https://cdn.animaapp.com/projects/6572e31b163d7a7791ee9861/releases/6572e371cd9388f80f786a26/img/download--8--removebg-preview--1--1.png"
+          />
+        </div>
+        <p className="powered-by-hudai">
+          <span className="span">Powered By</span>
+          <span className="text-wrapper-7"> hudAI</span>
+        </p>
+      </div>
+    </div>
+  );
+};
